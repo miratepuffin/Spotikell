384f86a6d8fbd560ed0cdb76e1c713a8761cad18
MiratePuffin <b.a.steer@se12.qmul.ac.uk>
2015-12-05 22:11:15 +0000

    Formatted output into tables

--- SpotifyDatabaseAccess.hs
+++ SpotifyDatabaseAccess.hs
@@ -3,6 +3,7 @@ module SpotifyDatabaseAccess (createPageTest) where
 import Control.Monad
 import Database.HDBC
 import Database.HDBC.MySQL
+import Data.List.Split
 import SpotifyDataTypes
 
 mySQLInfo :: MySQLConnectInfo
@@ -55,35 +56,71 @@ createPageTest artist = do
     idArtist   <- getArtistID artist
     albumPairs <- getAlbumInfo idArtist
     albumText  <- outputAlbums albumPairs
-    let html = unlines ["<html><head/><body>",
+    let html = unlines ["<html>",
+                        createHeader,
+                        "<body>",
+                        "<p><center><h1>",artist," Album Preview","</h1></center></p>",
+                        "<div id=\"accordion\">",
                         albumText,
                         "</body></html>"]
-    writeFile (artist++".html") html
 
+    let htmlRedux = rmvBlankLines $ splitOn "\n" html
+    writeFile (artist++".html") htmlRedux
+
+createHeader :: String
+createHeader = unlines ["<head>",
+  "<meta charset=\"utf-8\">",
+  "<title>CHANGE CHANGE CHAGE</title>",
+  "<link rel= \"stylesheet\" href=\"jquery-ui.css\">",
+  "<script src=\"http://code.jquery.com/jquery-1.10.2.js\"></script>",
+  "<script src=\"http://code.jquery.com/ui/1.11.4/jquery-ui.js\"></script>",
+  --"<link rel=\"stylesheet\" href=\"/resources/demos/style.css\">",
+  "<script>",
+  "$(function() {",
+  "$( \"#accordion\" ).accordion();",
+  "$(\"#accordion\").show().accordion({autoHeight: false});",
+  "$(\"#accordion div\").css({ 'height': 'auto' });",
+  "$( \"#accordion\" ).accordion({collapsible: true});",
+  "$( \"#accordion\" ).accordion({active: false});",
+  "});",
+  "</script>",
+  "</head>"] 
 outputAlbums:: [(Int,String)] -> IO String
 outputAlbums [] = return ""
 outputAlbums (album:albums) = do 
     trackPairs <- getTrackInfo (fst album)
     remainingAlbums <- outputAlbums albums
     albumArtwork <- getImageURL (fst album)
-    return $ unlines ["<p><h1><center>",
+    return $ unlines ["<h3 class=\"album\">",
+                      "<div class=\"innerDiv\"><img src=\"",albumArtwork,"\" height=\"20%\">",
                       (snd album),
-                      "</h1></center></P>",
-                      "<p><center><img src=\"",
-                      albumArtwork,
-                      "\" alt=\"Album image\">",
-                      "</h1></center></P>",
-                      (outputTracks trackPairs),
+                      "</div></h3><div><center><table cellspacing=\"100pt\" cellpadding=\"40pt\">",
+                      (outputTracks trackPairs 1),
+                      "</table></center></div>",
                       remainingAlbums]
 
-outputTracks:: [(String,String)] -> String
-outputTracks [] = ""
-outputTracks (track:tracks) = unlines ["<p><h2>",
-                                       (fst track),
-                                       "</h2></p>",
-                                       "<audio controls> <source src=\"",
-                                       (snd track),
-                                       "\" type=\"audio/mpeg\">",
-                                       "Your browser does not support the audio element.",
-                                       "</audio>",
-                                       (outputTracks tracks)]
+--outputTracks:: [(String,String)] -> String
+--outputTracks [] = ""
+--outputTracks (track:tracks) = unlines ["<p><h2>",
+--                                       (fst track),
+--                                       "</h2></p>",
+--                                       "<audio controls> <source src=\"",(snd track),"\" type=\"audio/mpeg\">",
+--                                       "</audio>",
+--                                       (outputTracks tracks)]
+
+outputTracks:: [(String,String)] -> Int -> String
+outputTracks [] _ =  "</tr>"
+outputTracks (track:tracks) rowCount | rowCount == 1 = "<tr><td>"++(insideTD track) ++ "</td>"      ++ (outputTracks tracks 2)
+outputTracks (track:tracks) rowCount | rowCount == 2 = "<td>"    ++(insideTD track) ++ "</td>"      ++ (outputTracks tracks 3)
+outputTracks (track:tracks) rowCount | rowCount == 3 = "<td>"    ++(insideTD track) ++ "</td></tr>" ++ (outputTracks tracks 1) 
+
+
+insideTD track = unlines ["<p><center><h4>",
+                           (fst track),
+                           "</h4></p>",
+                           "<audio controls> <source src=\"",(snd track),"\" type=\"audio/mpeg\">",
+                           "</center></audio>"]
+
+rmvBlankLines :: [String] -> String
+rmvBlankLines lines = unlines $ filter notBlank lines 
+                        where notBlank a = not ((a == "")|| (a =="\r")|| (a =="\n"))
