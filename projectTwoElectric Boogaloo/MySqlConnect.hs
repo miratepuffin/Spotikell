08879421cbe5f95e0c07151cf28fb944a9b90c8b
MiratePuffin <b.a.steer@se12.qmul.ac.uk>
2015-12-15 17:55:45 +0000

    Forgot to change the DB

--- projectTwoElectric Boogaloo/MySqlConnect.hs
+++ projectTwoElectric Boogaloo/MySqlConnect.hs
@@ -1,8 +1,17 @@
 module MySqlConnect (getConnection, closeConnection) where
 import Database.HDBC
 import Database.HDBC.MySQL
-
+--local data
 mySQLInfo :: MySQLConnectInfo
+mySQLInfo = defaultMySQLConnectInfo {
+  mysqlHost = "localhost",
+  mysqlDatabase = "spotify",
+  mysqlUser = "root",
+  mysqlPassword = "1234",
+  mysqlUnixSocket = "/var/run/mysqld/mysqld.sock" 
+}
+--remote database on amazon
+mySQLInfo' :: MySQLConnectInfo
 mySQLInfo = defaultMySQLConnectInfo {
   mysqlHost = "spotikell.cp3m1q1ydb0h.eu-west-1.rds.amazonaws.com",
   mysqlDatabase = "spotify",
